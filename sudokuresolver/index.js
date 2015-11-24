9a964dadf49006dfc8ce5650177ec14465fba3ae
dieguitofernandez <dieguitofernandez@gmail.com>
2015-11-23 22:06:06 -0300

    Primer commit

---
+++ sudokuresolver/index.js
@@ -0,0 +1,45 @@
+﻿$(function () {
+    $('#goLink').on('click', Start);
+});
+
+function Start() {
+    //Clear content
+    var mainContainer = $('#mainContainer');
+    mainContainer.html('');
+
+    var totalThreads = $('#threadsInput').val();
+    for (var i = 0; i < totalThreads; i++) {
+        mainContainer.append('<div class="SolutionContainer">');
+    }
+
+    $('.SolutionContainer').each(TrySolution);
+}
+
+function TrySolution() {
+    var myContainer = this;
+    var board = [   [0, 0, 1, 1, 1, 0, 0],
+                    [0, 0, 1, 1, 1, 0, 0],
+                    [1, 1, 1, 1, 1, 1, 1],
+                    [1, 1, 1, 0, 1, 1, 1],
+                    [1, 1, 1, 1, 1, 1, 1],
+                    [0, 0, 1, 1, 1, 0, 0],
+                    [0, 0, 1, 1, 1, 0, 0]];
+
+    RenderBoard(board, myContainer);
+}
+
+function RenderBoard(board, container) {
+    //Draw table in container
+    var output = '<table>';
+    for (var i = 0; i < 7; i++) {
+        output+='<tr>';
+        for (var j = 0; j < 7; j++) {
+            output+='<td>';
+            output+=board[i][j];
+            output+='</td>';
+        }
+        output+='</tr>';
+    }
+    output+='</table>';
+    $(container).html(output);
+}
